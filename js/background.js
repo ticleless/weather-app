11f3416a3d6f894bc926f394d27497cde2cf683f
ticleless <kyy20802@gmail.com>
2023-06-26 02:35:19 +0900

    first

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg"
+]
+
+const chosenImage = images[(Math.floor(Math.random() * images.length))];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+// document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(${bgImage.src})`;
+document.body.style.backgroundRepeat = "no-repeat";
---
+++ js/greetings.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
